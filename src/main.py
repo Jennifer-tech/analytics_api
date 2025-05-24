182f9b3cd025cf4da7deff7bd886569a5e93b6ec
Chioma Jennifer <muofunanyajennifer1@gmail.com>
2025-05-24 20:56:03 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from typing import Union
 
 from fastapi import FastAPI
-from fastapi.midddleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from api.db.session import init_db
 from api.events import router as event_router
 
